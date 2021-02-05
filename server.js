e9e5ba19e671876ed0667c0c60862d3a967cc83f
willholmeswastaken <willholmes97@icloud.com>
2021-02-05 15:52:10 +0000

    Added support for env vars locally

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const path = require('path');
 const gateway = require('express-gateway');
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+console.log(process.env);
+
 gateway()
   .load(path.join(__dirname, 'config'))
   .run();
